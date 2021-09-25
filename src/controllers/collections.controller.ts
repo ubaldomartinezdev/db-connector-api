3bd220e13bbc668b1a96499dc29266a27dd5f3d7
umanalytics <umartin@wgu.edu>
2021-09-25 07:55:10 -0500

    update all

--- src/controllers/collections.controller.ts
+++ src/controllers/collections.controller.ts
@@ -12,12 +12,12 @@ router.get("/", (_, res: Response) =>
   )
 );
 
-// const SELECT_COLLECTION = "SELECT * FROM items WHERE contract = $1";
-// router.get("/:contract", (req: Request, res: Response) =>
-//   db.query(SELECT_COLLECTION, [req.params.contract]).then(
-//     ({ rows }) => res.status(200).json(rows),
-//     (error) => res.status(500).send(error.message)
-//   )
-// );
+const SELECT_COLLECTION = "SELECT * FROM items WHERE contract = $1";
+router.get("/:contract", (req: Request, res: Response) =>
+  db.query(SELECT_COLLECTION, [req.params.contract]).then(
+    ({ rows }) => res.status(200).json(rows),
+    (error) => res.status(500).send(error.message)
+  )
+);
 
 export { router as collections };
