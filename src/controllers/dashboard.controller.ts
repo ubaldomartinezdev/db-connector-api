4f1b398c27e24c28ae49bf7dda8f9741317ea7e5
umanalytics <umartin@wgu.edu>
2021-10-13 04:15:15 -0500

    updated query for last_sale_price

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -13,7 +13,7 @@ router.get("/", (_, res: Response) =>
 );
 
 const SELECT_LAST_SALE_PRICE =
-    "SELECT contract_address, token_id, last_sale_price, last_sale_date FROM last_sale_price WHERE contract_address = $1";
+    "SELECT contract_address, token_id, last_sale_price, last_sale_date FROM last_sale_price WHERE contract_address = $1 AND last_sale_date != '-1' ORDER BY last_sale_date DESC";
 router.get("/:contract_address/last_sale_price", (req: Request, res: Response) =>
     db.query(SELECT_LAST_SALE_PRICE, [req.params.contract_address]).then(
         ({ rows }) => res.status(200).json(rows),
