e22fafe40bf239277f012c7a3913e81fedb6b068
umanalytics <umartin@wgu.edu>
2021-10-13 02:01:55 -0500

    added last_sale_price query

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -12,4 +12,15 @@ router.get("/", (_, res: Response) =>
     )
 );
 
+const SELECT_LAST_SALE_PRICE =
+    "SELECT token_id, last_sale_price, last_sale_date FROM last_sale_price WHERE contract_address = $1";
+router.get("/:contract_address/last_sale_price", (req: Request, res: Response) =>
+    db.query(SELECT_LAST_SALE_PRICE, [req.params.contract_id]).then(
+        ({ rows }) => res.status(200).json(rows),
+        (error) => res.status(500).send(error.message)
+    )
+);
+
+
+
 export { router as dashboard };
