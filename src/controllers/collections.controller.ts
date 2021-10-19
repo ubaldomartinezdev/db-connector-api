001f95fd9a91fa04fd3af4874fb7967dbf58eec5
umanalytics <umartin@wgu.edu>
2021-10-19 02:02:48 -0500

    Changed column names in database & updated queries to reflect database. contract_id TO contract_address name TO collection_name

--- src/controllers/collections.controller.ts
+++ src/controllers/collections.controller.ts
@@ -4,7 +4,7 @@ import { db } from "../db";
 
 const router = Router();
 
-const SELECT_COLLECTIONS = "SELECT id, name, contract_id FROM collections";
+const SELECT_COLLECTIONS = "SELECT id, collection_name, contract_address FROM collections";
 router.get("/", (_, res: Response) =>
   db.query(SELECT_COLLECTIONS).then(
     ({ rows }) => res.status(200).json(rows),
@@ -13,27 +13,27 @@ router.get("/", (_, res: Response) =>
 );
 
 const SELECT_COLLECTION =
-  "SELECT token_id FROM bot_list WHERE contract_id = $1 ORDER BY floor_price";
-router.get("/:contract_id", (req: Request, res: Response) =>
-  db.query(SELECT_COLLECTION, [req.params.contract_id]).then(
+  "SELECT token_id FROM bot_list WHERE contract_address = $1";
+router.get("/:contract_address", (req: Request, res: Response) =>
+  db.query(SELECT_COLLECTION, [req.params.contract_address]).then(
     ({ rows }) => res.status(200).json(rows),
     (error) => res.status(500).send(error.message)
   )
 );
 
 const SELECT_SELL_PRICE =
-  "SELECT sell_price FROM collections WHERE contract_id = $1";
-router.get("/:contract_id/sell_price", (req: Request, res: Response) =>
-  db.query(SELECT_SELL_PRICE, [req.params.contract_id]).then(
+  "SELECT sell_price FROM collections WHERE contract_address = $1";
+router.get("/:contract_address/sell_price", (req: Request, res: Response) =>
+  db.query(SELECT_SELL_PRICE, [req.params.contract_address]).then(
     ({ rows }) => res.status(200).json(rows),
     (error) => res.status(500).send(error.message)
   )
 );
 
 const SELECT_BID_PRICE =
-  "SELECT bid_price FROM collections WHERE contract_id = $1";
-router.get("/:contract_id/bid_price", (req: Request, res: Response) =>
-  db.query(SELECT_BID_PRICE, [req.params.contract_id]).then(
+  "SELECT bid_price FROM collections WHERE contract_address = $1";
+router.get("/:contract_address/bid_price", (req: Request, res: Response) =>
+  db.query(SELECT_BID_PRICE, [req.params.contract_address]).then(
     ({ rows }) => res.status(200).json(rows),
     (error) => res.status(500).send(error.message)
   )
