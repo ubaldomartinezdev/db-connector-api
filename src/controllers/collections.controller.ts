99fc706e5a0edb0c137129f756a1af84bb48c765
Nikita Sherbakov <nikita@sherbakov.dev>
2021-10-24 01:05:56 +0300

    Add label to req

--- src/controllers/collections.controller.ts
+++ src/controllers/collections.controller.ts
@@ -4,7 +4,8 @@ import { db } from "../db";
 
 const router = Router();
 
-const SELECT_COLLECTIONS = "SELECT id, collection_name, contract_address FROM collections";
+const SELECT_COLLECTIONS =
+  "SELECT id, collection_name, contract_address FROM collections";
 router.get("/", (_, res: Response) =>
   db.query(SELECT_COLLECTIONS).then(
     ({ rows }) => res.status(200).json(rows),
@@ -14,11 +15,13 @@ router.get("/", (_, res: Response) =>
 
 const SELECT_COLLECTION =
   "SELECT token_id FROM bot_list WHERE contract_address = $1";
-router.get("/:contract_address", (req: Request, res: Response) =>
-  db.query(SELECT_COLLECTION, [req.params.contract_address]).then(
-    ({ rows }) => res.status(200).json(rows),
-    (error) => res.status(500).send(error.message)
-  )
+router.get("/:contract_address/:label", (req: Request, res: Response) =>
+  db
+    .query(SELECT_COLLECTION, [req.params.contract_address, req.params.label])
+    .then(
+      ({ rows }) => res.status(200).json(rows),
+      (error) => res.status(500).send(error.message)
+    )
 );
 
 const SELECT_SELL_PRICE =
